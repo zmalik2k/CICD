a8521fd98d1f1cf4e55393b5eac5aef7c96ed9bb
zmalik2k <zmalik2k@hotmail.com>
2025-02-27 17:52:11 -0800

    added terraform.tfvars

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = "~> 1.10.0"
   backend "s3" {
-    bucket = "tsanghan-tfstate"
+    bucket = "zmalik-tfstate"
     key    = "f5xc-cicd.tfstate"
-    region = "ap-southeast-1"
+    region = "us-west-2"
   }
 }
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+domains                  = "zmalik.dev.learnf5.cloud"
+name                     = "zmalik"
+namespace                = "zmalik"
+origin_pool_service_name = "boutique-frontend.zmalik"
+origin_pool_virtual_site = "zmalik-vsite"
